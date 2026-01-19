bef9630e200aeb537f192effee3983605455bf4c
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-19 13:00:28 +0530

    change

--- models/User.ts
+++ models/User.ts
@@ -1,3 +1,5 @@
+'use server';
+
 import mongoose, {Schema, model, models} from "mongoose";
 import bcrypt from "bcryptjs";
 
@@ -22,10 +24,11 @@ const userSchema = new Schema<IUser>(
 
 );
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next: any) {
     if (this.isModified("password")) {
       this.password = await bcrypt.hash(this.password, 10); 
     }
+    next();
 });
 
 const  User = models?.User || model<IUser>("User", userSchema)
